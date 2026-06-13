453c7786c70e49b77598e1e80ef27c665eab315a
Coby Shelton <cobyshelton@Cobys-MacBook-Air.local>
2026-06-12 21:05:44 -0500

    togModal

---
+++ index.js
@@ -0,0 +1,36 @@
+// template_7eg5zjh
+// service_ev57qhh
+// yPA_waBBdiEo_-lHM
+function contact(event){
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += " modal__overlay--visible";
+    emailjs
+        .sendForm(
+            'service_ev57qhh',
+            'template_7eg5zjh',
+            event.target,
+            'yPA_waBBdiEo_-lHM'
+        ).then(() => {
+            loading.classList.remove("modal__overlay--visible");
+            success.classList += " modal__overlay--visible";
+        }).catch(() => {
+            loading.classList.remove("modal__overlay--visible");
+            alert(
+                "The email service is temporarily unavailable. Please contact me directly at cobyshelton17@gmail.com."
+            );
+        });
+}
+
+let isModalOpen = false;
+function toggleModal() {
+    if (isModalOpen){
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open");
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open";
+}
+
+// 7:37 Toggling Modal
